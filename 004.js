706a02615b73ae4b4e984e74003dcffed14aad6c
Cauê Coser <cauecoser@gmail.com>
2023-01-24 19:49:14 -0300

    Aprimoramento da solução do exercício prático da aula 04 do módulo de JS.

--- 004.js
+++ 004.js
@@ -1,11 +1,12 @@
-let caractere = prompt('Digite uma letra do alfabeto:')
+let caractere = prompt('Digite uma letra do alfabeto:').toLowerCase()
 
-if (isNaN(caractere)) {
+if (isNaN(caractere) && caractere.length==1) {
     if(caractere=='a'||caractere=='e'||caractere=='i'||caractere=='o'||caractere=='u') {
         alert('A letra digitada é uma vogal.')
     } else {
         alert('A letra digitada é uma consoante.')
     }
 } else {
-    alert('[ERRO] Digite uma letra para obter o resultado.')
+    alert('[ERRO] Digite uma letra, e apenas uma, para obter o resultado.')
+    window.location.reload()
 }
