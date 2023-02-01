b8aef1c07d2211f3e4b8983ca9583cb5e9e1ecc4
Cauê Coser <cauecoser@gmail.com>
2023-01-31 21:39:48 -0300

    Ayualizando exercício prático da aula 02 do múdulo do JS intermediário.

--- JS_interm/002.js
+++ JS_interm/002.js
@@ -1,19 +1,19 @@
 let tabela = [[2, 0, 0], [2, 1, 2], [2, 2, 4], [2, 3, 6], [2, 4, 8], [2, 5, 10], [2, 6, 12], [2, 7, 14], [2, 8, 16], [2, 9, 18], [2, 10, 20]]
 
 // let indice = 2
-// let linhas = []
+// let tabela = []
 // let colunas = []
 
-// for (i = 0; i <= 10; i++) {
+// for (i = 0; i <= 10; i++) { 
 //     colunas.push(indice)
 //     colunas.push(i)
 //     colunas.push(indice * i)
+//     tabela.push(colunas) 
+//     colunas = []
 // }
+// console.log(tabela)
 
-// linhas.push(colunas)
-// alert(linhas)
-
-alert('A tabela acima representa, logicamente a tabuada do número 2.')
+alert('A tabela acima representa, matematicamente, a tabuada do número 2.')
 
 alert('Cheguei a essa conclusão entendendo que cada um dos números de 0 a 10 está posicionado sequenciadamente e que cada um dos seus sucessores em cada linha da matriz informa o dobro dele. A partir disso, vendo que o número 2 se repete em todas as linhas, deduzi que é a tabuada do 2.')
 
