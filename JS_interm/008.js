c4ce446a7ff53dfa5e8eb727ca4d5da2fc221f2c
Cauê Coser <cauecoser@gmail.com>
2023-02-13 21:21:59 -0300

    Resolução do exercpicio prático da aula 08 do módulo de JS intermediário.

---
+++ JS_interm/008.js
@@ -0,0 +1,24 @@
+let checaNumeroDeElementosDoBody = (quantidadeDeFilhosDoBody) => {
+    if (quantidadeDeFilhosDoBody > 5) {
+        return true
+    } else {
+        return false
+    }
+}
+
+let exibeTextosDosParagrafosDoBody = (paragrafos) => {
+    paragrafos.forEach(element => {
+        console.log(element.outerText)
+    })
+}
+
+let filhosDeBody = document.body.childElementCount
+console.log(filhosDeBody)
+
+console.log(checaNumeroDeElementosDoBody(filhosDeBody))
+
+// let paragrafosDoBody = document.querySelectorAll('p')
+let tagsPDoBody = document.querySelectorAll('p')
+console.log(tagsPDoBody)
+
+exibeTextosDosParagrafosDoBody(tagsPDoBody)
